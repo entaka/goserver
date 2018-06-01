f825421379c36cafc5ef93755cd5fb6b897a5013
s-crew_endo <t.endo@s-crew.jpn.com>
2018-06-01 14:28:56 +0900

    web.go メイン処理コメント追加

--- web.go
+++ web.go
@@ -30,6 +30,21 @@ func testFunc(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	// http.Handler httpルート用Handlerインターフェース
+	// serveHTTP(ResponseWriter,*Request) を定義している -> Handlerとして定義
+
+	// http.HandleFunc( pattern string, handler func ) ※よく使う
+	// パターンと関数を渡してルートを指定する
+	// 関数の形式は上記で定義したHandlerの型に合わせる
+
+	// http.Handle( pattern string, handler Handler) ※よく使う
+	// パターンとHandlerを渡してデフォルトルートとして指定する
+	// http.ListenAndServe(":9090", nil)
+	//  のように第2引数をnilにした場合自動でHandlerとして登録される
+
+	// http.HandlerFunc httpルート用HandlerHunc関数
+	// HandlerFunc型でfuncを指定すればキャストの必要なくHandlerとして使用できるようになる
+
 	http.HandleFunc("/", sayhelloName)  //アクセスのルーティングを設定します。
 	http.HandleFunc("/test/", testFunc) // テスト用
 	controller.Controller()
